aaf455450a82b4460c27a171758fddc6ac244e13
sanchan <danilo.dev@gmail.com>
2017-04-22 13:00:06 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# React Awesome Placeholder
+Awesome loading placeholders for React (like Facebook's post placeholder)
+
+<img src="https://raw.githubusercontent.com/sanchan/react-awesome-placeholder/screenshots/01.png"/>
---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React                from 'react'
+import Navigation           from 'components/react-awesome-navigation'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    }
+  }
+
+  toggleMenu() {
+    this.setState((prevState, props) => ({
+      open: !this.state.open
+    }))
+  }
+
+  render() {
+    const Menu = (
+      <div>
+        Menu
+      </div>
+    )
+
+    const Content = (
+      <div>
+        <button onClick={this.toggleMenu.bind(this)}>Open</button>
+      </div>
+    )
+
+    return (
+      <Navigation
+        open={this.state.open}
+        menu={Menu}
+        content={Content}
+        enterDuration={'500ms'}
+        leaveDuration={'500ms'}
+      />
+    );
+  }
+}
+export default App
